6bd654bec880a85db958ed54b4e4fb5b21d9ab94
Andres Montoya-Aristizabal <andres@triplean.com>
2023-10-29 19:54:20 -0400

    Updated navbar and display of audio items

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -20,10 +20,22 @@ export default async function Home() {
     }
 
     return (
-    <div>
-        <AuthButton/>
-        <AudioRecorder/>
-        {audioItems && <RealtimeAudioItems audioItems={audioItems}/>}
+    <div className="flex items-center flex-col w-full gap-5">
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+            <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+                <p className="text-2xl lg:text-2xl items-center justify-center !leading-tight font-bold">Audio Helper</p>
+                <AuthButton/>
+            </div>
+        </nav>
+        <div className="flex flex-col items-center gap-12">
+            <AudioRecorder/>
+            {audioItems && 
+                <div className="flex items-center flex-col gap-4">
+                    <p className="text-3xl lg:text-4xl !leading-tight text-center font-bold hover:underline">Recorded Audio</p>
+                    <RealtimeAudioItems audioItems={audioItems}/>
+                </div>
+            }
+        </div>
     </div>
     )
 }

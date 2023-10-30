54da85d3c7d99eac86702d6ed45235ffdf0db724
Andres Montoya-Aristizabal <andres@triplean.com>
2023-10-30 01:46:19 -0400

    Add disclaimer comment

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,12 @@
 
 export default function Header() {
   return (
-    <div className="flex flex-col gap-10 items-center">
+    <div className="flex flex-col gap-5 items-center">
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
         We take notes for your meetings so you dont have to.
+      </p>
+      <p className="text-3x1 sm:text !leading-tight mx-auto max-w-xl text-center underline">
+        Note: this website does not work on phones.
       </p>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
